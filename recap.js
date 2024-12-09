e798f7271643952101d25ba1a104340eda09da94
tanmaykaushikk <kaushik.tanmay11@gmail.com>
2024-12-09 11:05:15 +0530

    revision of array methods

---
+++ recap.js
@@ -0,0 +1,30 @@
+//recap
+// const arr = [1,2,3,4,5];
+// console.log(arr.length);
+
+// to String 
+// console.log(arr.toString());
+
+// join
+// console.log(arr.join('|'));   // join in place of comma
+
+//
+const brr = [10,11,12,13];
+// arr.concat(brr);
+
+
+const arr = [1,2,3,4,4,5,6,7,7,7,8,9,10,10,11];
+
+arr.sort();
+let ans = [];
+for(let i=0;i<arr.length;i++){
+    if(arr[i] === arr[i+1]){
+        ans.push(arr[i]);
+    }
+}
+
+console.log(ans);
+
+
+
+
