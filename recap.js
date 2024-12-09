6052aba2350dd922c6d8bc4411945e0e0e46112c
tanmaykaushikk <kaushik.tanmay11@gmail.com>
2024-12-09 13:30:46 +0530

    recap

--- recap.js
+++ recap.js
@@ -9,21 +9,36 @@
 // console.log(arr.join('|'));   // join in place of comma
 
 //
-const brr = [10,11,12,13];
+// const brr = [10,11,12,13];
 // arr.concat(brr);
 
 
-const arr = [1,2,3,4,4,5,6,7,7,7,8,9,10,10,11];
+// const arr = [1,2,3,4,4,5,6,7,7,7,8,9,10,10,11];
 
-arr.sort();
-let ans = [];
-for(let i=0;i<arr.length;i++){
-    if(arr[i] === arr[i+1]){
-        ans.push(arr[i]);
-    }
-}
+// arr.sort();
+// let ans = [];
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i] === arr[i+1]){
+//         ans.push(arr[i]);
+//     }
+// }
 
-console.log(ans);
+// console.log(ans);
+
+
+// let emp = {
+//     Name: 'Tanmay',
+//     lastName : 'Kaushik',
+//     salary: 40000
+// };
+
+// console.log(delete emp.salary);
+// console.log(emp);
+
+
+
+
+
 
 
 
