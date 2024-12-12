1c6faa9faccc7fc458e2275571c8ddfe7ec3640a
tanmaykaushikk <kaushik.tanmay11@gmail.com>
2024-12-12 11:17:32 +0530

    NFE

---
+++ NFE.js
@@ -0,0 +1,28 @@
+// Named Function Expression
+
+//syntax
+// const myFunction = function funName(){
+// };
+
+
+// basic example
+// const myFunc = function seyHello(){
+//     console.log("hello!");
+//     console.log(typeof seyHello);   // function - accessible within the function
+// }
+
+// myFunc();
+// console.log(typeof sayHello);   // undefined - not accessible outside
+
+
+// //recursion
+// const factorial = function fact(n){
+//     if(n<=1) return 1;
+//     return n*fact(n-1);    
+// };
+
+// console.log(factorial(5));
+// console.log(typeof fact);      // undefined fact is not accessible here
+
+
+
